51bb9551e19bf4626025af5b0f3ac15d757ffa87
kaiwenwang233 <51180520+kaiwenwang233@users.noreply.github.com>
2022-02-25 18:46:19 -0500

    Update writepicks.py

--- writepicks.py
+++ writepicks.py
@@ -3,12 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as sio
 import glob
-files = [f for f in glob.glob('/data/beroza/kaiwenw/Geysers/FilmDataDetection/test_pred/results/' + "*.npz", recursive=True)]
-file = open('/data/beroza/kaiwenw/Geysers/FilmDataDetection/test_pred/picksfile.txt','a') 
+files = [f for f in glob.glob('./test_pred/results/' + "*.npz", recursive=True)]
+file = open('./test_pred/picksfile.txt','a') 
 for ifile in range(len(files)):
-    preds=np.load('/data/beroza/kaiwenw/Geysers/FilmDataDetection/test_pred/results/'+files[ifile].split('/')[-1][0:-4]+'.npz')
+    preds=np.load('./test_pred/results/'+files[ifile].split('/')[-1][0:-4]+'.npz')
     ind=detect_peaks(preds['pred'][0,:,1],mph=0.5,mpd=10)
-    pslabel=sio.loadmat('/data/beroza/kaiwenw/Geysers/STEAD/STEADtraining/'+files[ifile].split('/')[-1][0:-4])['pslabel']
+    pslabel=sio.loadmat('./STEADtraining/'+files[ifile].split('/')[-1][0:-4])['pslabel']
     labels=np.zeros(387)
     if pslabel.shape[1]==4:
         labels[np.min(pslabel[:,0]):np.max(pslabel[:,1])]=1
